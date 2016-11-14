83429a6de418f423053fce40e6e66985f94e34a2
MaxZeeHazy <maxim.zh@outlook.com>
2016-11-14 15:23:22 +0200

    Home Work b1l2

--- src/main/java/com/javaonlinecourse/b1lesson3/homework/HW02.java
+++ src/main/java/com/javaonlinecourse/b1lesson3/homework/HW02.java
@@ -10,6 +10,23 @@ package com.javaonlinecourse.b1lesson3.homework;
  */
 public class HW02 {
     public static void main(String[] args) {
+        int min = 1,
+                max = 99,
+                count = -1;
+        int [] arr = new int[max%2==0 ? max/2 : max/2+1];
+//
+        String strSorted = "",
+                strReversed = "";
+
+//
+        while(count < arr.length-1)
+        {
+            arr[++count] = min;
+            strSorted += min +" ";
+            strReversed = min +" "+ strReversed;
+            min += 2;
+        }
+        System.out.println(strReversed);
 
     }
 }
--- src/main/java/com/javaonlinecourse/b1lesson3/homework/HW03.java
+++ src/main/java/com/javaonlinecourse/b1lesson3/homework/HW03.java
@@ -1,5 +1,8 @@
 package com.javaonlinecourse.b1lesson3.homework;
 
+import java.util.Arrays;
+import java.util.Collections;
+
 /**
  * @author emitrohin
  * @version 1.0
@@ -11,6 +14,16 @@ package com.javaonlinecourse.b1lesson3.homework;
  */
 public class HW03 {
     public static void main(String[] args) {
-
+        int[] Mas = new int[8];
+        for(int i=0;i<Mas.length;i++){
+            Mas[i]=(int)(Math.random()*9+1);
+            System.out.print(Mas[i] +" ");
+        }
+        for(int i=0;i<Mas.length;i++){
+            if(Mas[i]%2!=0)Mas[i]=0;
+            System.out.println(Mas[i] +"");
+        }
     }
 }
+
+
