3d2b92e9af846c33cdec2beffaec54f7b86b6199
emitrohin <emitrohin@icloud.com>
2016-11-15 19:26:06 +0500

    cw 01

---
+++ src/main/java/com/javaonlinecourse/b2lesson1/classwork/TestGit.java
@@ -0,0 +1,12 @@
+package com.javaonlinecourse.b2lesson1.classwork;
+
+/**
+ * @author emitrohin
+ * @version 1.0
+ *          15.11.2016
+ */
+public class TestGit {
+    public static void main(String[] args) {
+
+    }
+}
